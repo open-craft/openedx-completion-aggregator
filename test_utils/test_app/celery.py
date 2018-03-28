d478e8c8581cdc317edc555179894c11384e6a01
J. Cliff Dyer <jcd@sdf.org>
2018-03-28 14:17:55 -0400

    Use JSON in celery communication.

--- test_utils/test_app/celery.py
+++ test_utils/test_app/celery.py
@@ -10,5 +10,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_settings')
 
-app = Celery('test_project', broker='redis://')  # pylint: disable=invalid-name
+app = Celery('test_project',
+    broker='redis://')  # pylint: disable=invalid-name
+app.conf.update(accept_content=['json'])
 app.autodiscover_tasks(['completion_aggregator'])
