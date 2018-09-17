6974f24d08abbbb8fb124d67ef5d48eb8f5499a1
J. Cliff Dyer <jcd@sdf.org>
2018-09-17 16:48:12 -0400

    Bump version to 1.3

--- completion_aggregator/__init__.py
+++ completion_aggregator/__init__.py
@@ -4,6 +4,6 @@ an app that aggregates block level completion data for different block types for
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '1.2'
+__version__ = '1.3'
 
 default_app_config = 'completion_aggregator.apps.CompletionAggregatorAppConfig'  # pylint: disable=invalid-name
