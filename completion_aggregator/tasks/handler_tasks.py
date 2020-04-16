da0ec73b3e96229919e6a0e4ce6ccffe2c6b32f8
Paulo Viadanna <paulo@opencraft.com>
2020-04-16 09:21:40 -0300

    Convert text_type back to CourseKey

--- completion_aggregator/tasks/handler_tasks.py
+++ completion_aggregator/tasks/handler_tasks.py
@@ -7,6 +7,7 @@ from celery import shared_task
 from celery_utils.logged_task import LoggedTask
 
 from django.conf import settings
+from opaque_keys.edx.keys import CourseKey
 
 from ..batch import perform_aggregation
 from ..cachegroup import CacheGroup
@@ -20,6 +21,8 @@ def mark_all_stale(course_key, users=None):
     Mark the specified enrollments as stale for all blocks.
     """
     users = users or get_active_users(course_key)
+    if isinstance(course_key, six.text_type):
+        course_key = CourseKey.from_string(course_key)
     stale_objects = [StaleCompletion(username=user.username, course_key=course_key, force=True) for user in users]
     StaleCompletion.objects.bulk_create(stale_objects, batch_size=1000)
     CacheGroup().delete_group(six.text_type(course_key))
