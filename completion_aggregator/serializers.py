c98a579535dffdee9770f5ac5ae28463612d91de
Sohaib Aslam <sohaib.aslam@arbisoft.com>
2020-07-02 15:12:57 +0500

    MCKIN-17580 Excluded users with roles from mean calculation.

--- completion_aggregator/serializers.py
+++ completion_aggregator/serializers.py
@@ -258,7 +258,9 @@ class CourseCompletionSerializer(serializers.Serializer):
         """
         Caclulate mean completion percent for all enrolled users.
         """
-        enrollments = compat.get_users_enrolled_in(obj.course_key)
+        enrollments = compat.get_users_enrolled_in(obj.course_key).exclude(
+            courseaccessrole__course_id=obj.course_key
+        )
         enrollment_count = enrollments.count()
         if enrollment_count == 0:
             return 0.
@@ -266,6 +268,8 @@ class CourseCompletionSerializer(serializers.Serializer):
         total = Aggregator.objects.filter(
             course_key=obj.course_key,
             aggregation_name='course',
+        ).exclude(
+            user__courseaccessrole__course_id=obj.course_key
         ).aggregate(
             total=Sum(Coalesce('percent', Value(0.)))
         ).get('total') or 0.
