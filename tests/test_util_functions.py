a4c46ff92aab0e432d8f5b093b28c7e2f3e9724d
Paulo Viadanna <paulo@opencraft.com>
2021-02-19 08:32:33 -0300

    Fix Django version check (#82)

--- tests/test_util_functions.py
+++ tests/test_util_functions.py
@@ -6,12 +6,15 @@ Tests for the `openedx-completion-aggregator` utils module.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from datetime import datetime, timezone
+from unittest.mock import patch
+
 import ddt
 import pytest
 
 from django.test import TestCase
 
-from completion_aggregator.utils import get_percent
+from completion_aggregator.utils import get_percent, make_datetime_timezone_unaware
 
 
 @ddt.ddt
@@ -32,3 +35,21 @@ class GetPercentTestCase(TestCase):
     def test_get_percent_with_valid_values(self, earned, possible, expected_percentage):
         percentage = get_percent(earned, possible)
         self.assertEqual(percentage, expected_percentage)
+
+
+@ddt.ddt
+class MakeTimeZoneUnawareTestCase(TestCase):
+    """
+    Tests of the `make_datetime_timezone_unaware` function
+    """
+
+    @ddt.data(
+        (1, 10, 'a1'),
+        (1, 10),
+        (2, 0, 'a1'),
+        (2, 2),
+    )
+    def test_make_datetime_timezone_unaware(self, version):
+        with patch('django.VERSION', version):
+            date = make_datetime_timezone_unaware(datetime.now(timezone.utc))
+            assert date.tzinfo is None
